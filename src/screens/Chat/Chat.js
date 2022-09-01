e6ccc9e8b842f23a8e889aab89c15c42fac76fd9
Furkan Bagirgan <furkanbrgn@gmail.com>
2022-09-01 19:43:28 +0300

    edit chat screen

--- src/screens/Chat/Chat.js
+++ src/screens/Chat/Chat.js
@@ -1,15 +1,58 @@
-import moment from 'moment';
-import React from 'react';
-import {Text, SafeAreaView} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {SafeAreaView, View, Text, FlatList, StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import styles from './Chat.style';
+import { useUser } from '../../contexts/UserContext';
+import Input from '../../components/Input';
+import Message from '../../components/Message';
 
-const Chat = () => {
-  moment.locale('tr');
+const Chat = ({route}) => {
+  //Here, the parameters from the home screen are taken by route and assigned to the objects.
+  const {receiver,type} = route.params;
+  const {currentUser,chatList}=useUser();
+  const [message,setMessage]=useState('');
+  const [list,setList]=useState([]);
 
+  useEffect(()=>{
+    let messages={};
+    chatList.forEach((value,index)=>{
+      if(value.receiver.userName===receiver.userName){
+        messages={...value};
+      }
+    })
+    setList(messages.messageList);
+  },[chatList]);
+
+  //Here is the function where key assignments of the fields to repeat in the flatlist are made.
+  const keyExtractor = (item, index) => {
+    return String(index);
+  };
+
+  //Here, there is a function that adjusts how the areas to be repeated in the
+  //flatlist will appear on the screen. Also, a MessageCard component is created for each message.
+  const renderItem = ({item}) => {
+    return <Message message={item} />;
+  };
+
+  //Here, messages are displayed on the screen with the textInput field and flatlist at the bottom.
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Chat Screen</Text>
+      <FlatList
+        keyExtractor={keyExtractor}
+        data={list}
+        renderItem={renderItem}
+        overScrollMode="never"
+        bounces={false}
+      />
+      <View style={styles.bottomContainer}>
+        <View style={styles.textInput}>
+          <Input placeholder='Write a message' placeholderTextColor='#8C8C8C' value={message} onChangeText={setMessage}/>
+        </View>
+        <View style={styles.sendButton}>
+          <Icon name="send" size={23} color="white" />
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
--- src/screens/Chat/Chat.style.js
+++ src/screens/Chat/Chat.style.js
@@ -3,7 +3,54 @@ import {StyleSheet} from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'gray'
+    backgroundColor: '#ebe5de',
+    paddingTop: 15,
+  },
+  bottomContainer: {
+    width: '100%',
+    height: 80,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 15,
+    paddingTop: 20,
+    paddingBottom: 15,
+  },
+  textInput: {
+    width: '83%',
+    height: '100%',
+    paddingLeft: 10,
+    paddingRight: 15,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderRadius: 25,
+    backgroundColor: 'white',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  sendButton: {
+    width: 45,
+    height: 45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 22.5,
+    backgroundColor: '#128C7E',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
 });
 
