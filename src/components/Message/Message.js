027f52a3aa2a7d807c7ae3533e0116b08b7efed6
Furkan Bagirgan <furkanbrgn@gmail.com>
2022-09-01 19:43:15 +0300

    add message component

---
+++ src/components/Message/Message.js
@@ -0,0 +1,40 @@
+import moment from 'moment';
+import React from 'react';
+import {View, Text} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import styles from './Message.style';
+
+const Message = ({message}) => {
+  //Here, the location is entered into the moment library to display the dates and times in different formats.
+  moment.locale('tr');
+
+  //Here, messages are created by checking the status of the sender via the incoming message prop.
+  return (
+    <View
+      style={
+        message.sender === 'itself'
+          ? styles.sendContainer
+          : styles.receiveContainer
+      }>
+      <View
+        style={
+          message.sender === 'itself' ? styles.rightCorner : styles.leftCorner
+        }
+      />
+      <View style={styles.messageWrapper}>
+        <Text style={styles.message}>{message.message}</Text>
+      </View>
+      <View style={styles.timeWrapper}>
+        <Text style={styles.time}>
+          {moment(message.datetime).format('hh:mm')}
+        </Text>
+        {message.sender === 'itself' && (
+          <Icon name="check" size={15} color="black" style={styles.icon} />
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default Message;
---
+++ src/components/Message/Message.style.js
@@ -0,0 +1,82 @@
+import {StyleSheet} from 'react-native';
+
+//Here the basic styles of the messageCard are created.
+const basicStyles = StyleSheet.create({
+  container: {
+    padding: 10,
+    maxWidth: '65%',
+    borderRadius: 15,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginBottom: 15,
+  },
+});
+
+////Here the styles of the messageCard are created.
+const styles = StyleSheet.create({
+  sendContainer: {
+    ...basicStyles.container,
+    backgroundColor: '#e0f6ca',
+    alignSelf: 'flex-end',
+    marginRight: 15,
+  },
+  receiveContainer: {
+    ...basicStyles.container,
+    backgroundColor: 'white',
+    alignSelf: 'flex-start',
+    marginLeft: 15,
+  },
+  messageWrapper: {
+    flexDirection: 'row',
+    flexShrink: 1,
+  },
+  message: {
+    color: 'black',
+  },
+  timeWrapper: {
+    alignSelf: 'flex-end',
+    marginTop: 10,
+    marginLeft: 8,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  time: {
+    fontSize: 11,
+    color: 'black',
+  },
+  icon: {
+    marginLeft: 3,
+  },
+  rightCorner: {
+    position: 'absolute',
+    bottom: 0,
+    right: -8,
+    width: 0,
+    height: 0,
+    borderTopWidth: 30,
+    borderBottomWidth: 0,
+    borderLeftWidth: 20,
+    borderStyle: 'solid',
+    backgroundColor: 'transparent',
+    borderBottomColor: 'transparent',
+    borderTopColor: 'transparent',
+    borderLeftColor: '#e0f6ca',
+  },
+  leftCorner: {
+    position: 'absolute',
+    bottom: 0,
+    left: -8,
+    width: 0,
+    height: 0,
+    borderTopWidth: 30,
+    borderBottomWidth: 0,
+    borderRightWidth: 20,
+    borderStyle: 'solid',
+    backgroundColor: 'transparent',
+    borderBottomColor: 'transparent',
+    borderTopColor: 'transparent',
+    borderRightColor: 'white',
+  },
+});
+
+export default styles;
