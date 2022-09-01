84865c1902b6e309286ab944b95a6b81442a9aa2
Furkan Bagirgan <furkanbrgn@gmail.com>
2022-09-01 15:16:40 +0300

    edit login screen

--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -1,14 +1,46 @@
-import React from 'react';
-import {Text, SafeAreaView} from 'react-native';
+import React,{useState} from 'react';
+import {SafeAreaView,View} from 'react-native';
+import {Picker} from '@react-native-picker/picker';
 
 import styles from './Login.style';
+import Input from '../../components/Input'
+import CountryCodes from '../../CountryCodes.json';
 
 const Login = () => {
+  const [selectedPhoneCode, setSelectedPhoneCode] = useState('+90');
+  const [phoneNumber,setPhoneNumber]=useState('');
+  const [firstName,setFirstName]=useState('');
+  const [lastName,setLastName]=useState('');
+  const [userName,setUserName]=useState('');
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Login Screen</Text>
+      <View style={styles.rowContent}>
+        <View style={styles.pickerWrapper}>
+          <Picker
+            selectedValue={selectedPhoneCode}
+            style={styles.picker}
+            dropdownIconColor='#8C8C8C'
+            mode='dropdown'
+            onValueChange={(itemValue, itemPosition) =>
+              setSelectedPhoneCode(itemValue)
+            }>
+            {
+              CountryCodes.map((item,index)=> <Picker.Item key={index} label={item.code+' '+item.dial_code} value={item.dial_code} />)
+            }
+          </Picker>
+        </View>
+        <View style={styles.phoneWrapper}>
+          <Input placeholder="Phone Number" value={phoneNumber} onChangeText={setPhoneNumber} keyboardType='numeric'/>
+        </View>
+      </View>
+      <Input placeholder="First Name" placeholderTextColor='#8C8C8C' value={firstName} iconName="phone" onChangeText={setFirstName}/>
+      <Input placeholder="Last Name" placeholderTextColor='#8C8C8C' value={lastName} iconName="phone" onChangeText={setLastName}/>
+      <Input placeholder="User Name" placeholderTextColor='#8C8C8C' value={userName} iconName="phone" onChangeText={setUserName}/>
     </SafeAreaView>
   );
 };
+
+//<Button title="Giriş Yap" onClick={login} theme="secondary" loading={loading}/>
 
 export default Login;
--- src/screens/Login/Login.style.js
+++ src/screens/Login/Login.style.js
@@ -3,8 +3,35 @@ import {StyleSheet} from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'gray'
+    backgroundColor: 'white',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 25
   },
+  rowContent: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  pickerWrapper:{
+    flex: 8,
+    marginRight: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 50,
+    borderWidth: 1,
+    borderColor: '#8C8C8C',
+    borderRadius: 25,
+  },
+  picker:{
+    width: 150,
+    color: '#8C8C8C',
+  },
+  phoneWrapper: {
+    flex: 10
+  }
 });
 
 export default styles;
